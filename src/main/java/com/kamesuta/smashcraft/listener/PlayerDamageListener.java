529cd91d3842591f427f638240be58f6003246a9
Kamesuta <kamesuta@gmail.com>
2022-01-12 12:15:30 +0900

    ノックバックの計算

--- src/main/java/com/kamesuta/smashcraft/listener/PlayerDamageListener.java
+++ src/main/java/com/kamesuta/smashcraft/listener/PlayerDamageListener.java
@@ -3,13 +3,40 @@ package com.kamesuta.smashcraft.listener;
 import com.kamesuta.smashcraft.Config;
 import com.kamesuta.smashcraft.SmashCraft;
 import dev.kotx.flylib.ListenerAction;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.projectiles.ProjectileSource;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.util.Vector;
 
 public class PlayerDamageListener implements ListenerAction<EntityDamageEvent> {
+    private final Scoreboard sb;
+    private Objective objective;
+    private Objective objectivePercent;
+
+    public PlayerDamageListener() {
+        // 吹っ飛び率のスコアボードを追加
+        this.sb = Bukkit.getScoreboardManager().getMainScoreboard();
+
+        // 吹っ飛び率計算用
+        objective = this.sb.getObjective("futtobi");
+        if (objective == null)
+            objective = this.sb.registerNewObjective("futtobi", "dummy", Component.text("吹っ飛び率"));
+
+        // 吹っ飛び率表示用
+        objectivePercent = this.sb.getObjective("futtobi_per");
+        if (objectivePercent == null)
+            objectivePercent = this.sb.registerNewObjective("futtobi_per", "dummy", Component.text("%"));
+    }
+
     @Override
     public void execute(EntityDamageEvent e) {
         // SmashCraftモードがONのときのみ
@@ -23,6 +50,7 @@ public class PlayerDamageListener implements ListenerAction<EntityDamageEvent> {
         }
 
         // 奈落 または /kill 以外ならダメージ0
+        double damage = e.getDamage();
         if (e.getCause() != EntityDamageEvent.DamageCause.VOID) {
             // ダメージは0
             e.setDamage(0);
@@ -31,18 +59,51 @@ public class PlayerDamageListener implements ListenerAction<EntityDamageEvent> {
         // 人に殴られたときのみ吹っ飛ぶ
         if (e instanceof EntityDamageByEntityEvent) {
             EntityDamageByEntityEvent ev = (EntityDamageByEntityEvent) e;
+            final Entity damager = ev.getDamager();
+            Entity originalDamager = ev.getDamager();
+
+            // 弓やトライデント
+            if (originalDamager instanceof Projectile) {
+                ProjectileSource shooter = ((Projectile) originalDamager).getShooter();
+                if (shooter instanceof Player)
+                    originalDamager = (Player) shooter;
+            }
 
             // 人に殴られたときのみ
-            if (!(ev.getDamager() instanceof Player)) {
+            if (!(originalDamager instanceof Player)) {
                 return;
             }
 
-            // ノックバックの方向 = 殴った人の顔の向き
-            Vector direction = ((Player) ev.getDamager()).getEyeLocation().getDirection();
+            // 吹っ飛び率
+            Score score = objective.getScore(e.getEntity().getName());
+            // ダメージ量を吹っ飛び率に加算
+            score.setScore(score.getScore() + (int) damage);
+
+            // 強さ
+            double knockbackCoefficient = score.getScore();
+
+            // ノックバックの方向
+            Vector knockback = knockbackDirection(damager, e.getEntity(), knockbackCoefficient);
+            Vector direction = knockback.clone().normalize();
+            double force = knockback.length();
 
             // 吹っ飛ぶ
-            new SetVelocityTask(((Player) e.getEntity()), direction, 1).runTaskTimerAsynchronously(SmashCraft.instance, 0, 4);
+            new SetVelocityTask((Player) e.getEntity(), direction, force)
+                    .runTaskTimerAsynchronously(SmashCraft.instance, 0, 4);
         }
+    }
+
+    // ノックバックの方向
+    private Vector knockbackDirection(Entity damager, Entity entity, double force) {
+        // Minecraftのノックバック挙動を再現
+        double x = Math.sin(damager.getLocation().getYaw() * 0.017453292F);
+        double z = -Math.cos(damager.getLocation().getYaw() * 0.017453292F);
+
+        Vector vec3d = entity.getVelocity();
+        Vector vec3d2 = (new Vector(x, 0.0D, z)).normalize().multiply(force);
+        return new Vector(vec3d.getX() / 2.0D - vec3d2.getX(),
+                entity.isOnGround() ? Math.min(0.4D, vec3d.getY() / 2.0D + force) : vec3d.getY(),
+                vec3d.getZ() / 2.0D - vec3d2.getZ());
     }
 }
 
@@ -51,12 +112,9 @@ class SetVelocityTask extends BukkitRunnable {
     private final int numberOfTimes;
     private final Player target;
     private final Vector vector;
-    // 吹っ飛び率
-    private final float knockbackCoefficient;
 
-    SetVelocityTask(Player target, Vector direction, float knockbackCoefficient) {
+    SetVelocityTask(Player target, Vector direction, double knockbackCoefficient) {
         this.target = target;
-        this.knockbackCoefficient = knockbackCoefficient;
         this.numberOfTimes = ((int) Math.ceil(knockbackCoefficient / 4));
 
         if (knockbackCoefficient > 4) {
